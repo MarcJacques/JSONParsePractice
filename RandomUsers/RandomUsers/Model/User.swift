9dbe137e81cfa34136948a542a6d5d6e7750e290
Marc Jacques <Marcjacques@me.com>
2019-09-05 20:37:52 -0400

    first commit

---
+++ RandomUsers/RandomUsers/Model/User.swift
@@ -0,0 +1,19 @@
+//
+//  User.swift
+//  RandomUsers
+//
+//  Created by Marc Jacques on 9/5/19.
+//  Copyright © 2019 Mitchell Budge. All rights reserved.
+//
+
+import Foundation
+
+struct UserResult: Decodable {
+    let result: [User] = [] //because everything in the APi is in an array called results
+}
+
+struct User: Decodable {
+    
+    let name: String
+    let
+}
